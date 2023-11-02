f9b008fd8cdfcf239bc3e9cb925e46aa2cdcbb93
dmitriy <damrin.dima@mail.ru>
2023-11-02 20:06:43 +0500

    testone

---
+++ Controller.py
@@ -0,0 +1,26 @@
+import ui
+
+
+def run():
+    input_from_user = ''
+    while input_from_user != '7':
+        ui.menu()
+        input_from_user = input().strip()
+        if input_from_user == '1':
+            function.show('all')
+        if input_from_user == '2':
+            function.add()
+        if input_from_user == '3':
+            function.show('all')
+            f.id_edit_del_show('del')
+        if input_from_user == '4':
+            function.show('all')
+            function.id_edit_del_show('edit')
+        if input_from_user == '5':
+            function.show('date')
+        if input_from_user == '6':
+            function.show('id')
+            f.id_edit_del_show('show')
+        if input_from_user == '7':
+            ui.goodbuy()
+            break
---
+++ Function.py
@@ -0,0 +1,59 @@
+import Operation
+import Note
+import ui
+
+number = 6  # сколько знаков МИНИМУМ может быть в тексте заметки
+
+
+def add():
+    note = ui.create_note(number)
+    array = Operation.read_file()
+    for notes in array:
+        if Note.Note.get_id(note) == Note.Note.get_id(notes):
+            Note.Note.set_id(note)
+    array.append(note)
+    Operation.write_file(array, 'a')
+    print('Заметка добавлена...')
+
+
+def show(text):
+    logic = True
+    array = Operation.read_file()
+    if text == 'date':
+        date = input('Введите дату в формате dd.mm.yyyy: ')
+    for notes in array:
+        if text == 'all':
+            logic = False
+            print(Note.Note.map_note(notes))
+        if text == 'id':
+            logic = False
+            print('ID: ' + Note.Note.get_id(notes))
+        if text == 'date':
+            logic = False
+            if date in Note.Note.get_date(notes):
+                print(Note.Note.map_note(notes))
+    if logic == True:
+        print('Нет ни одной заметки...')
+
+
+def id_edit_del_show(text):
+    id = input('Введите id необходимой заметки: ')
+    array = Operation.read_file()
+    logic = True
+    for notes in array:
+        if id == Note.Note.get_id(notes):
+            logic = False
+            if text == 'edit':
+                note = ui.create_note(number)
+                Note.Note.set_title(notes, note.get_title())
+                Note.Note.set_body(notes, note.get_body())
+                Note.Note.set_date(notes)
+                print('Заметка изменена...')
+            if text == 'del':
+                array.remove(notes)
+                print('Заметка удалена...')
+            if text == 'show':
+                print(Note.Note.map_note(notes))
+    if logic == True:
+        print('Такой заметки нет, возможно, вы ввели неверный id')
+    Operation.write_file(array, 'a')
